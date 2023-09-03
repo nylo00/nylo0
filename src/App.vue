7a949ff04734200857514c0aa3cda3f38882da5a
Nylo00 <nylo0@yahoo.com>
2023-09-03 17:43:38 +0200

    push main
    push main

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+
+<script>
+function touch(){
+    document.getElementById("bubble").innerHTML = "pls don't touch me I doesn't like getting touched!";
+ }
+ </script>
+
+<template>
+  <header>
+    
+    <div class="wrapper">
+      <nav>
+      <RouterLink to="/"><button data-text="Awesome" class="button">
+        <span class="actual-text">&nbsp;Nylo0&nbsp;</span>
+        <span class="hover-text" aria-hidden="true">&nbsp;Nylo0&nbsp;</span>
+      </button>
+      </RouterLink>
+     <div class="sozial-links"><a class="socials" href="https://github.com/nylo00"><img style="width: 50px;" src="./assets/svg/githubl.svg"></a></div>
+      </nav>
+      <nav>
+        <RouterLink class="header-link" to="/">Home</RouterLink>
+        <RouterLink class="header-link" to="/mySkills">my Skills</RouterLink>
+        
+      </nav>
+    </div>
+    
+  </header>
+
+  <RouterView />
+
+ 
+
+
+  <div class="nerd-place">
+    <p id="bubble">pls don't touch me I doesn't like getting touched! </p>
+      
+   <button style="background-color: transparent; border-color: transparent;" @click="touch()"><img class="nerd" src="./assets/svg/geekhead.png"></button>
+    <button id="sorry" class="sorry" @click="sorry()">i'm Sorry</button>
+  </div>
+</template>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+
+function touch(){
+    var bubble = document.getElementById("bubble");
+    var sorry = document.getElementById("sorry");
+    if(bubble.style.display != "none")
+    {
+      bubble.style.display = "block";
+      sorry.style.display = "block";
+    } 
+ }
+ function sorry(){
+  var bubble = document.getElementById("bubble");
+    var sorry = document.getElementById("sorry");
+    if(bubble.style.display == "block")
+    {
+      bubble.style.display = "none";
+      sorry.style.display = "none";
+    } 
+ }
+
+
+  
+ 
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import MySkills from "@/views/MySkills.vue";
+</script>
+
+<template>
+  <body >
+    <div class="slider"></div>
+    <div class="main-page-content">
+      <div class="main-card">
+        <div class="pb-box">
+          <!--Portfolio PB-->
+        <img style=" width: 50px;" src="../assets/svg/geekhead.png">
+        <!--Portfolio PB-->
+       </div>
+        <div>
+          <!--text-->
+          <p style="text-align: center; margin: 5% 5%;">Hi, my Name is Silas an I am coding since I was 13 years old. I started by coding plugins for minecraft in java and than switched to php and created webapplication just for fun an experience</p>
+           <!--text-->
+           <!--Links-->
+
+           <!--Links-->
+           
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
